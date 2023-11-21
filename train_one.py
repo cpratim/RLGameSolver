1ed5b6e094ba84e2538b0bd2341258c489bb7815
Hardik Gupta <hardikgupta13@outlook.com>
2023-11-21 06:50:34 +0530

    Plot generation in the One agent training model.

--- train_one.py
+++ train_one.py
@@ -1,10 +1,10 @@
 import numpy as np
 from environment.one_payoff_matrix import OnePayoffMatrix 
 from solver.agent import QLearningAgentLSTM
-from solver.brain import LSTMBrain, LinearBrain, RNNBrain
+from solver.brain import LinearBrain, RNNBrain
 from warnings import filterwarnings
-from time import sleep
-import torch
+from matplotlib import pyplot as plt
+
 
 filterwarnings('ignore')
 
@@ -28,47 +28,85 @@ random_payoff_matrix = np.array([
     [-1, 4, 3]
 ])
 
+target_dist_blotto = np.array([0.44444,0.44444,0,0,0.11111])
 
-def get_random_dist(n):
-    dist = np.random.rand(n)
-    dist /= dist.sum()
-    return dist
+target_dist_rock_paper_scissors = np.array([0.33333,0.33333,0.33333])
 
-target_dist = np.array([0.16667,0.27778,0.55556])
+target_dist_random = np.array([0.55,0.35,0.1])
 
-def train(agent, env, num_episodes=100000, log_interval=1000, tolerance=1e-3, round_size=5):
+def train(agent, env,target_dist, num_episodes=100000, log_interval=1000, tolerance=1e-3, round_size=5):
+
+    episodes = []
+    distances = []
+    plt.figure()
+    plt.figure()
+    plt.title('Convergence of Agent Distribution to Target Distribution')
+    plt.xlabel('Episode')
+    plt.ylabel('Distance')
 
     for episode in range(num_episodes):
         state = env.reset()
         done = False
-        loss_items = []
-        average_reward = []
 
         while not done:
 
             agent_distribution = agent.get_agent_distribution(state)
-
             agent_actions = np.random.choice(
                 env.payoff_matrix.shape[0],
                 round_size, p=agent_distribution
             )
 
             states = [env.get_state()] * round_size
-            # print(states)
             _, reward, done, _ = env.step(agent_distribution, agent_actions)
-            loss = agent.update_model(states, agent_actions, reward)
-            # loss_items.append(loss)
-            # average_reward.append(reward)
-        
+            _ = agent.update_model(states, agent_actions, reward)
+
+            distance = np.linalg.norm(target_dist- np.array(agent_distribution))
+            episodes.append(episode)
+            distances.append(distance)
+
+            plt.plot(episodes, distances)
+            plt.draw()
+            plt.pause(0.00000000001)
+            if distance <= 0.01:
+                break
         if episode % log_interval == 0:
             print(f"Episode {episode}")
             print(f'Agent distribution: {agent_distribution}')
-        # sleep(1)
-
-
+            print("Distance: ", distance)
+            
+            
 if __name__ == '__main__':
 
-    env = OnePayoffMatrix(blotto_payoff_matrix)
+    print("Choose a case (1(Rock Paper Scissors), 2(Colonel Blotto), or 3(Random Matrix) or enter anything else to input your own matrix and target):")
+    case = int(input())
+
+    if case == 1:
+        payoff_matrix = rock_paper_scissors_payoff_matrix
+        target_dist = target_dist_rock_paper_scissors
+
+    elif case == 2:
+
+        payoff_matrix = blotto_payoff_matrix
+        target_dist = target_dist_blotto
+    elif case == 3:
+
+        payoff_matrix = random_payoff_matrix
+        target_dist = target_dist_random
+    else:
+        rows = int(input("Enter the number of rows in the matrix: "))
+        cols = int(input("Enter the number of columns in the matrix: "))
+        
+        print("Enter the elements of the 2D matrix (separate values with commas, press enter after each column):")
+        payoff_matrix = np.array([list(map(float, input().split(','))) for _ in range(rows)])
+        
+        print("Enter the target distance vector (space-separated values):")
+        target_dist = np.array(list(map(float, input().split())))
+        
+        if len(target_dist) != cols:
+            print("Error: The size of the target distance vector should match the number of columns in the matrix.")
+            exit()
+
+    env = OnePayoffMatrix(payoff_matrix)
 
     agent = QLearningAgentLSTM(
         env.action_space.n, 
@@ -76,7 +114,6 @@ if __name__ == '__main__':
         LinearBrain
     )
 
-
-    train(agent, env)
+    train(agent, env, target_dist)
     
     
